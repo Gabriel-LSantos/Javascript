01d1a83875a4fb29e4de4d30d04460a14eaee32b
Gabriel <96089511+Gabriel-LSantos@users.noreply.github.com>
2022-04-11 22:00:22 -0300

    Add files via upload

---
+++ trabalhandocomvariaveis.js
@@ -0,0 +1,14 @@
+console.log("trabalhandocomvariaveis");
+
+
+const idade = 22;
+let nome = "Gabriel";
+const sobrenome = "Lima";
+
+
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+nome = nome + sobrenome;
+console.log(nome);
+
+
